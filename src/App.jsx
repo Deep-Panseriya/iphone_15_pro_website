3b76272af14a4dd3e6d3a537f5644bf68ecba681
Panseriya <hardik.panseriya@hotmail.com>
2025-07-24 16:52:51 +0530

    Chip and game section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
 import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
 
 function App () {
   return (
@@ -14,6 +14,7 @@ function App () {
         <Highlights />
         <Model />
         <Features />
+        <HowItWorks />
       </main>
     </>
   )
